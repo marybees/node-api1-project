97ec5419875b4a28b4d9dcf10406e0ba8d827a72
Mary Baker <meb601@gmail.com>
2020-09-28 16:39:09 -0400

    GET request for /users

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const express = require("express");
 const server = express();
 
 server.use(express.json());
+
+server.get("/users", (req, res) => {
+  res.status(200).json({ data: users });
+});
+
+let users = [
+  {
+    id: "a_unique_id", // hint: use the shortid npm package to generate it
+    name: "Jane Doe",
+    bio: "Not Tarzan's Wife, another Jane",
+  },
+];
+
+const port = 5000;
+
+server.listen(port, () => console.log("api running"));
+
+// npm run server
